0467eb812eae847953c40beac58ec6acb68e4389
Juliusz Żakowski <90174540+juliuszakowski@users.noreply.github.com>
2023-11-12 23:21:25 +0100

    Add files via upload

---
+++ kik.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KiK</title>
+    <link rel="stylesheet" href="kik.css">
+</head>
+<body>
+
+<audio controls autoplay src='muzyka.mp3'></audio>
+
+<div class=naglowek>
+
+<h1>Kółko i krzyżyk</h1>
+
+</div>
+
+<div class=lewas>
+
+<h2>Wybierz tryb gry</h2>
+<button> z komputerem </button>
+<button> z innym graczem </button>
+<button> sieciówka </button>
+
+<h2>Wybierz wojownika</h2>
+<button>X</button>
+<button>O</button>
+
+<h2>Wyczyść planszę</h2>
+<button>Wyczyść</button>
+
+</div>
+
+<div class=gra>
+
+<table id=polagry>
+<tr> <td></td> <td></td> <td></td></tr>
+<tr> <td></td> <td></td> <td></td></tr>
+<tr> <td></td> <td></td> <td></td></tr>
+</table>
+
+</div>
+
+
+
+<div class=prawas>
+
+<h2>czasomierz/zegar</h2>
+
+<h2>historia wyników</h2>
+<img src="kratos.gif" alt="">
+</div>
+
+</body>
+</html>
