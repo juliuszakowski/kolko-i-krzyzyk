6481ec06db8c5150b478670724b8b6754d0c8889
Dominik Pykacz <90174596+WhaTheDogDoin@users.noreply.github.com>
2023-11-27 22:23:38 +0100

    Update kik.php

--- kik.php
+++ kik.php
@@ -8,8 +8,6 @@
 </head>
 <body>
 
-<audio controls autoplay src='muzyka.mp3'></audio> <!-- muza -->
-
 <div class="naglowek">
     <img src="naglowek.png" alt="" width="500" height="300">
 </div>
@@ -23,6 +21,25 @@
     <h2>Wybierz wojownika</h2> <!-- wybierasz X albo O -->
     <button onclick="chooseSymbol('X')">X</button>
     <button onclick="chooseSymbol('O')">O</button>
+
+    <!-- muza -->
+    <p> og nutka gow </p>
+    <button class="grajmuza"> Graj </button>
+    <button class="pauzamuza"> Pauza </button>
+    <button class="stopmuza"> Stop </button>
+
+    <audio class="muz" src="GOW5.mp3"></audio>
+
+    <p> jakas nuta nw co to </p>
+    <button class="grajmuza2"> Graj </button>
+    <button class="pauzamuza2"> Pauza </button>
+    <button class="stopmuza2"> Stop </button>
+
+    <audio class="muz2" src="forever.mp3"></audio>
+
+    <p> Głośność</p>
+    <input type="range" name="glosnosc" id="glos1">
+    <span class="num">100</span>
 
     <h2>Wyczyść planszę</h2> <!-- czyszczenie planszy -->
     <button onclick="clearBoard()">Wyczyść</button>
@@ -49,6 +66,8 @@
     </table>
 </div>
 
+
+
 <div class="prawas">
     <h2>zegar</h2>
 <p id="timer">Czas: 00:00</p>
@@ -69,6 +88,7 @@
 </div>
 
 <script>
+    // zmienne 
     let currentPlayer;
     let board = ['', '', '', '', '', '', '', '', ''];
     let gameActive = false;
@@ -76,6 +96,8 @@
     let xWins = 0;
     let oWins = 0;
     let draws = 0;
+
+    // funkcaj do startowania
     function startGame(mode) {
         console.log(`Rozpoczęto grę w trybie: ${mode}`);
         gameMode = mode;
@@ -90,6 +112,8 @@
         }
     }
 
+
+    // Wybor O albo X do gry
     function chooseSymbol(symbol) {
         console.log(`Wybrano symbol: ${symbol}`);
         currentPlayer = symbol;
@@ -99,6 +123,8 @@
         }
     }
 
+
+    // sprawdzanie czy gra dalej trwa i czy jest winner albo remis 
     function makeMove(row, col) {
         if (!gameActive || board[row * 3 + col] !== '') {
             return;
@@ -130,6 +156,7 @@
         }
     }
 
+    // AI kompa do gry
     function makeComputerMove() {
         const emptyCells = board.reduce((acc, cell, index) => {
             if (cell === '') {
@@ -149,6 +176,7 @@
         }
     }
 
+    // czyszczenie planszy
     function clearBoard() {
         console.log('Wyczyszczono planszę');
         board = ['', '', '', '', '', '', '', '', ''];
@@ -161,11 +189,12 @@
         document.getElementById('instructions').innerText = 'Kliknij pole, aby postawić swój symbol.';
     }
 
+    //winner pozycje
     function checkWinner() {
         const winPatterns = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-            [0, 4, 8], [2, 4, 6]             // Diagonals
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+            [0, 4, 8], [2, 4, 6]             
         ];
 
         for (const pattern of winPatterns) {
@@ -178,6 +207,7 @@
         return null;
     }
 
+    
     function checkDraw() {
         return !board.includes('');
     }
@@ -195,6 +225,8 @@
             cell.appendChild(img);
         });
     }
+    
+    // Czas gry 
     function updateTimer() {
         const currentTime = new Date();
         const elapsedSeconds = Math.floor((currentTime - startTime) / 1000);
@@ -205,6 +237,8 @@
         document.getElementById('timer').innerText = `Czas: ${formattedTime}`;
         setTimeout(updateTimer, 1000); // Aktualizacja co sekundę
     }
+
+        //historia gry
         function updateScore(winner) {
         if (winner === 'X') {
             xWins++;
@@ -215,6 +249,66 @@
         document.getElementById('xWins').innerText = xWins;
         document.getElementById('oWins').innerText = oWins;
         document.getElementById('draws').innerText = draws;
+    }
+
+    // nuta1 i nuta2 graj pauza sciszanie
+    let play = document.querySelector('.grajmuza')
+    let play2 = document.querySelector('.grajmuza2')
+    let audio = document.querySelector('.muz')
+    let audio2 = document.querySelector('.muz2')
+    let vol = document.querySelector('#glos1')
+    let num = document.querySelector('.num')
+    let pause = document.querySelector('.pauzamuza')
+    let stopp = document.querySelector('.stopmuza')
+    let pause2 = document.querySelector('.pauzamuza2')
+    let stopp2 = document.querySelector('.stopmuza2')
+
+    //glosnosc
+    vol.oninput = function() {
+    audio.volume = vol.value/100
+    num.innerHTML = vol.value;
+    audio2.volume = vol.value/100
+    num.innerHTML = vol.value;
+    }
+
+    //granie 
+    play.addEventListener('click', ()=> {
+    audio.play();
+    console.log('1 gra');
+    })
+    play2.addEventListener('click', ()=> {
+    audio2.play();
+    console.log('2 gra')
+    })
+
+    //sranie
+    pause.addEventListener('click', ()=> {
+    audio.pause();
+    console.log('1 pauza');
+    })
+    pause2.addEventListener('click', ()=> {
+    audio2.pause();
+    console.log('2 pauza');
+    })
+
+    //stopowanie
+    stopp.addEventListener('click', ()=> {
+    audio.pause();
+    audio.currentTime = 0;
+    console.log('1 stop');
+    })
+    stopp2.addEventListener('click', ()=> {
+    audio2.pause();
+    audio2.currentTime = 0;
+    console.log('2 stop');
+    })
+
+    //gra tylko 1 piosenka
+    audio.onplay = function() {
+    audio2.pause();
+    }
+    audio2.onplay = function() {
+    audio.pause();
     }
 </script>
 
